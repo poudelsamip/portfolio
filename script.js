dbc217d39a3dcee0f4a979b7a2c51ad4c753bda1
Samip Poudel <144611884+poudelsamip@users.noreply.github.com>
2023-12-07 13:33:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// Get the form element by its ID
-var myForm = document.getElementById('myForm');
 
-// Get the button element by its ID
+var myForm = document.getElementById('myForm');
 var sendButton = document.getElementById('send_btn');
 
-// Add a click event listener to the button
-sendButton.addEventListener('click', function(event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
+        function showSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            const menubtn = document.querySelector('.menubtn')
+            sidebar.style.display = 'flex'
+            menubtn.style.display = 'none'
 
-    // Check if the form is valid
+        }
+        function hideSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'none'
+            const menubtn = document.querySelector('.menubtn')
+            menubtn.style.display = 'flex'
+        }
+
+sendButton.addEventListener('click', function(event) {
+    event.preventDefault();
     if (myForm.checkValidity()) {
-        // If the form is valid, submit it programmatically to the Google Form
-        myForm.action = "https://docs.google.com/forms/u/3/d/e/1FAIpQLSdRmUfwFWQTJPqR1Q70KsW22J_v2jobQSjV9fJzFWbeBo0bJw/formResponse"; // Replace with your Google Form URL
+        
+        myForm.action = "https://docs.google.com/forms/u/3/d/e/1FAIpQLSdRmUfwFWQTJPqR1Q70KsW22J_v2jobQSjV9fJzFWbeBo0bJw/formResponse";
         myForm.method = "POST";
         myForm.submit();
-        
-        // Optionally, you can display a success message
-        
+
         myForm.reset();
         alert('Message sent successfully.');
     } else {
-        // If the form is not valid, display an error message
+        
         alert('Fill all the places correctly.');
     }
 });
